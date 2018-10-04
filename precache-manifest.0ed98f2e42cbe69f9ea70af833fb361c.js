3314e4afc4fb67d3350a88435f4b9f5c78687b2b
Tru Huynh <hctru68@gmail.com>
2018-10-04 15:03:20 +0700

    Updates

---
+++ precache-manifest.0ed98f2e42cbe69f9ea70af833fb361c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/game_tictactoe/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c827362c206518ae6ce",
+    "url": "/game_tictactoe/static/js/runtime~main.8c827362.js"
+  },
+  {
+    "revision": "e38980970617ea894bed",
+    "url": "/game_tictactoe/static/js/main.e3898097.chunk.js"
+  },
+  {
+    "revision": "a0a4c40b8ea4537c9260",
+    "url": "/game_tictactoe/static/js/1.a0a4c40b.chunk.js"
+  },
+  {
+    "revision": "e38980970617ea894bed",
+    "url": "/game_tictactoe/static/css/main.dbff34d1.chunk.css"
+  },
+  {
+    "revision": "de206f284e7f0cefab32460392854b66",
+    "url": "/game_tictactoe/index.html"
+  }
+];
